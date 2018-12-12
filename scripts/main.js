70fce45b843b0295fed6e8d3d6f02f771afdddca
WanyiChen <wanyi.chen@swim.it>
2018-12-12 16:36:16 -0500

    load deals

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
 const EXMP = $rdf.Namespace('https://example.org/fragments/');
+const SCMA = $rdf.Namespace('http://schema.org/');
 
 var store = $rdf.graph();
 var fetcher = new $rdf.Fetcher(store);
@@ -20,15 +21,27 @@ $('#view').on("click", () => {
   loadShark($('#profile').val());
 });
 
+$('#shark').on('click', 'li', (e) => {
+  let target = $(e.target);
+  loadDeal(target.attr("id"), target.text());
+});
+
+$('#deal').on('click', 'li', (e) => {
+  let target = $(e.target);
+  loadShark(target.text());
+});
+
 async function loadShark(name) {
   $("#shark").show();
+  $("#deal").hide();
   if (sharks.includes(name)) {
     $("#error").hide();
     $("#name").show();
     $("#viewer").show();
     $("#name").text(name);
-    const sparqlQuery = 'SELECT ?uri WHERE { ?uri ?p "' + name + '" .}';
-    const query = $rdf.SPARQLToQuery(sparqlQuery, true, store);
+    $('#deals').empty();
+    const sharkQuery = 'SELECT ?uri WHERE { ?uri ?p "' + name + '" .}';
+    const query = $rdf.SPARQLToQuery(sharkQuery, true, store);
     store.query(query, function(result) {
       var uri = (result['?uri']).value;
       $('#wiki').text(uri);
@@ -38,10 +51,30 @@ async function loadShark(name) {
       $('#gender').text(gender.value);
       var country = store.any(shark, EXMP('country'));
       $('#country').text(country.value);
+      const dealQuery = 'SELECT ?dealName ?deal WHERE { ?deal <https://example.org/fragments/hasDealShark> <' + uri + '> . ?deal <http://schema.org/name> ?dealName.}';
+      const query2 = $rdf.SPARQLToQuery(dealQuery, true, store);
+      store.query(query2, function(result) {
+        $('#deals').append($('<li>').append(result['?dealName'].value).attr('id', result['?deal'].value));
+      });
     });
   } else {
     $("#error").show();
     $("#name").hide();
     $("#viewer").hide();
   }
+}
+
+async function loadDeal(uri, name) {
+  $("#shark").hide();
+  $("#deal").show();
+  $("#sharks").empty();
+  $("#dealName").text(name);
+  const deal = $rdf.sym(uri + '');
+  var pitch = store.any(deal, SCMA('description'));
+  $('#pitch').text(pitch.value);
+  const dealQuery = 'SELECT ?sharkName WHERE { <' + uri + '> <https://example.org/fragments/hasDealShark> ?shark . ?shark <http://xmlns.com/foaf/0.1/name> ?sharkName .}';
+  const query = $rdf.SPARQLToQuery(dealQuery, true, store);
+  store.query(query, function(result) {
+    $('#sharks').append($('<li>').append(result['?sharkName'].value));
+  });
 }
